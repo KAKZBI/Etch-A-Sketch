4779cf33af554b043d81b5ade8cb6e9e257e227a
KAKZBI <enelibrice@yahoo.fr>
2023-03-31 08:33:11 +0100

    The buttons black and eraser and the color input behave well

--- script.js
+++ script.js
@@ -7,15 +7,45 @@ container.style.gridTemplateColumns= 'repeat(16, 1fr)';
 // container.style.alignItems= 'center';
 // container.style.justifyContent= 'center';
 
+let actualColor = document.querySelector('#color-picker').value;
+window.addEventListener('load', initializeGridContainer);
+let black = document.querySelector('.black');
+black.addEventListener('click', ()=>changeHoverColor());
+let eraser = document.querySelector('.eraser');
+eraser.addEventListener('click', ()=>changeHoverColor('white'));
+document.querySelector('#color-picker').addEventListener('input', ()=>{
+    actualColor = document.querySelector('#color-picker').value;
+    changeHoverColor(actualColor);
+});
+
+
+function initializeGridContainer(){
 for(i=0; i < 16; i++) {
     for(j=0; j < 16; j++) {
     let inner_div = document.createElement('div') ;
     inner_div.classList.add('inner-div');
+    // inner_div.style.border = `${color} 1px solid`;
     container.appendChild(inner_div);
     
+    changeHoverColor(actualColor);
+    // console.log(actualColor);
     }
 }
+}
+
+
+function changeHoverColor(color='black'){
+    // console.log(color.target);
+    let inner_divs = document.querySelectorAll('.inner-div');
+    inner_divs.forEach(inner_div => inner_div.addEventListener('mouseover',()=>{
+        inner_div.style.backgroundColor = `${color}`;
+        // console.log(color);
+    }))
+}
+
 const gridConstructor =document.querySelector('#gridNumber');
+
+let inner_div = document.createElement('div') ;
 
 gridConstructor.addEventListener('change', changeGrid);
 
@@ -32,5 +62,5 @@ function changeGrid() {
         
         }
     }
-
+    changeHoverColor(actualColor);
 }
